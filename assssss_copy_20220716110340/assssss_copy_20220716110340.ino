486ee443423198bd3f43652bc754ea475383b51f
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-16 11:52:56 +0900

    Led 2개 추가

--- assssss_copy_20220716110340/assssss_copy_20220716110340.ino
+++ assssss_copy_20220716110340/assssss_copy_20220716110340.ino
@@ -1,11 +1,17 @@
 void setup() {
  pinMode(7,OUTPUT);
+  pinMode(10,OUTPUT);
+   pinMode(9,OUTPUT);
 }
 
 void loop() {
  
 digitalWrite(7,HIGH);
-delay(1000);
+digitalWrite(10,HIGH);
+digitalWrite(9,HIGH);
+delay(70);
 digitalWrite(7,LOW);
-delay(1000);
+digitalWrite(10,LOW);
+digitalWrite(9,LOW);
+delay(70);
 }
